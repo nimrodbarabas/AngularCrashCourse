594e34485dfe3c2fd014986e06aa989e4273de3d
unknown <nimrod_trb@yahoo.com>
2021-09-16 11:57:23 +0300

    Observable & Subsribe

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskService } from 'src/app/services/task.service';
 import { Task } from 'src/app/Task';
-import { TASKS } from 'src/app/mock-task';
+
 
 @Component({
   selector: 'app-tasks',
@@ -8,11 +9,12 @@ import { TASKS } from 'src/app/mock-task';
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
 
-  constructor() { }
+  constructor(private taskService:TaskService ) { }
 
   ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks)=>this.tasks=tasks);
   }
 
 }
